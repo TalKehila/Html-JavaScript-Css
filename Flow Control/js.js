7b550c9aa3ae4d6f3920d406feded054a8a97f49
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 15:36:19 +0300

    Exercise Flow control js

---
+++ Flow Control/js.js
@@ -0,0 +1,46 @@
+//1
+
+let n1 =2 , n2 = 3, n3 = 4;
+let res= n1 + n2 + n3;
+console.log(res);
+
+
+//2
+let s1='hello';
+let nu1 = 10;
+console.log(s1.length > nu1?  s1 : nu1 * 2);
+
+
+//3 
+let num1=20;
+let s1 = 'hellohhhhhhhhhhhhhh',s2 = 'worldddd';
+ if((s1.length+s2.length) >num1){
+    let x =5 ,y =2;
+    console.log(Math.max(x,y)); 
+    
+ }
+
+
+//4
+const str ="qwerty";
+switch(str.length){ 
+    case 3 : 
+        console.log("Three");
+        break;
+
+        case 7:
+            console.log("Seven");
+            break;
+            
+            default:
+                console.log("other");
+                break;
+        
+}
+
+//5 
+let x = 5;
+for(let i=0; i < x ; i++){
+console.log("hello");
+}
+
